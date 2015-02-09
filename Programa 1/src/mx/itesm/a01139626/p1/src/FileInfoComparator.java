f23d274bead0cc59a9291906d15ffdc656d65535
betoesquivel <ppbeto94@gmail.com>
2015-02-08 20:00:21 -0600

    Baby steps

---
+++ Programa 1/src/mx/itesm/a01139626/p1/src/FileInfoComparator.java
@@ -0,0 +1,5 @@
+package mx.itesm.a01139626.p1.src;
+
+public class FileInfoComparator implements ErrorMessages {
+
+}
---
+++ Programa 1/src/mx/itesm/a01139626/p1/src/FileLineSorter.java
@@ -0,0 +1,10 @@
+package mx.itesm.a01139626.p1.src;
+
+public class FileLineSorter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
